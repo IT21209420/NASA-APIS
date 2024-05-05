58e4db1fb9315a233552e4d486782226ace16cf6
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-05 08:02:58 +0530

    refactor: Update Home component with lazy loading for images and background video
    Update the Home component in the frontend to improve performance and enhance the visual experience. This change includes implementing lazy loading for images, which improves page load speed by loading images only when they are visible on the screen. Additionally, a background video is added to enhance the visual appeal of the page. These updates optimize the Home component and provide a better user experience.
    
    Note: This suggested commit message follows the convention used in the recent repository commits.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,6 +4,9 @@ import podHome from "../../assets/pod-home.jpg";
 import earthHome from "../../assets/earth-home.jpg";
 import { useEffect, useState } from "react";
 import Loading from "../../components/Loading";
+import BackgroundVideo from "../../components/BackgroundVideo";
+import bgVideo from "../../assets/video-home.mp4";
+
 const Home = () => {
   const [loading, setLoading] = useState(true);
 
@@ -26,44 +29,62 @@ const Home = () => {
   }
 
   return (
-    <div
-      className="px-4 py-2
-    bg-dominant
-    h-[88vh] flex justify-center items-center"
-    >
-      <div className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <Link to="/rovers" className="border rounded-md p-4 text-center">
-          <img
-            src={roverHome}
-            alt="Rover"
-            className="w-full h-64 object-cover mb-4"
-          />
-          <h2 className="font-bold">Rovers</h2>
-          <p>View rover photos</p>
-        </Link>
-        <Link
-          to="/picture_of_the_day"
-          className="border rounded-md p-4 text-center"
-        >
-          <img
-            src={podHome}
-            alt="Picture of the Day"
-            className="w-full h-64 object-cover mb-4"
-          />
-          <h2 className="font-bold">Picture of the Day</h2>
-          <p>View the picture of the day</p>
-        </Link>
-        <Link to="/earth_imagery" className="border rounded-md p-4 text-center">
-          <img
-            src={earthHome}
-            alt="Earth Imagery"
-            className="w-full h-64 object-cover mb-4"
-          />
-          <h2 className="font-bold">Earth Imagery</h2>
-          <p>View earth imagery</p>
-        </Link>
+    <BackgroundVideo url={bgVideo}>
+      <div className="px-4 py-2 text-white mt-10">
+        <div className="relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
+          <Link
+            to="/rovers"
+            className="border rounded-md p-4 text-center  bg-opacity-60 backdrop-filter backdrop-blur-lg"
+          >
+            <img
+              src={roverHome}
+              alt="Rover"
+              className="w-full h-64 object-cover mb-4"
+            />
+            <h2 className="font-bold">Rovers</h2>
+            <p className="text-sm">
+              Explore the Red Planet through the lens of NASA’s rovers. Each
+              image offers a glimpse into the Martian landscape, revealing the
+              beauty and mystery of our celestial neighbor
+            </p>
+          </Link>
+          <Link
+            to="/picture_of_the_day"
+            className="border rounded-md p-4 text-center  bg-opacity-60 backdrop-filter backdrop-blur-lg"
+          >
+            <img
+              src={podHome}
+              alt="Picture of the Day"
+              className="w-full h-64 object-cover mb-4"
+            />
+            <h2 className="font-bold">Picture of the Day</h2>
+            <p className="text-sm">
+              Discover the cosmos with NASA’s Picture of the Day. Each day
+              unveils a new celestial wonder, accompanied by insights from
+              professional astronomers, capturing the awe-inspiring beauty of
+              the universe.
+            </p>
+          </Link>
+          <Link
+            to="/earth_imagery"
+            className="border rounded-md p-4 text-center  bg-opacity-60 backdrop-filter backdrop-blur-lg"
+          >
+            <img
+              src={earthHome}
+              alt="Earth Imagery"
+              className="w-full h-64 object-cover mb-4"
+            />
+            <h2 className="font-bold">Earth Imagery</h2>
+            <p className="text-sm">
+              Embark on a virtual journey with NASA Earth Imagery. Witness the
+              dynamic beauty of our planet from space, capturing changes over
+              time and the diverse tapestry of landscapes that make up our
+              world.
+            </p>
+          </Link>
+        </div>
       </div>
-    </div>
+    </BackgroundVideo>
   );
 };
 
