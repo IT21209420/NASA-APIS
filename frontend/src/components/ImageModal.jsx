0b0d062a854d882d13cd2f0dcd6c64b6b814d7a8
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-05 16:47:33 +0530

    refactor: Add PropTypes to ImageModal component
    Add PropTypes to the ImageModal component in the frontend to ensure proper type checking and documentation. This change includes defining PropTypes for the src, modalIsOpen, closeModal, and full_name props, ensuring that the correct data types are passed to the component. This update improves code quality and maintainability.

--- frontend/src/components/ImageModal.jsx
+++ frontend/src/components/ImageModal.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Modal from "react-modal";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import PropTypes from "prop-types";
 
 const ImageModal = ({ src, modalIsOpen, closeModal, full_name }) => {
   return (
@@ -40,3 +40,25 @@ const ImageModal = ({ src, modalIsOpen, closeModal, full_name }) => {
 };
 
 export default ImageModal;
+
+ImageModal.propTypes = {
+  /**
+   * The source URL of the image to be displayed in the modal.
+   */
+  src: PropTypes.string.isRequired,
+
+  /**
+   * The state of the modal.
+   */
+  modalIsOpen: PropTypes.bool.isRequired,
+
+  /**
+   * Function to close the modal.
+   */
+  closeModal: PropTypes.func.isRequired,
+
+  /**
+   * The full name of the image to be displayed in the modal.
+   */
+  full_name: PropTypes.string,
+};
