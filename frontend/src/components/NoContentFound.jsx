4fef43177aa89ac66a2759650335f5aae7f2f5b0
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-05 16:47:50 +0530

    refactor: Update NoContentFound component to use prop types
    Update the NoContentFound component in the frontend to use prop types for type checking and documentation. This change includes adding PropTypes to the component, ensuring that the content prop is of type string and is required. By implementing prop types, we improve code quality and maintainability.

--- frontend/src/components/NoContentFound.jsx
+++ frontend/src/components/NoContentFound.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import PropTypes from "prop-types";
 
+/**
+ * Renders a component to display a message when no content is found.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.content - The content to be displayed.
+ * @returns {JSX.Element} The rendered component.
+ */
 const NoContentFound = ({ content }) => {
   return (
     <div className="flex items-center justify-center h-[58vh] w-full ">
-      <h1 className="text-4xl font-bold text-gray-700">{content}</h1>
+      <h1 className="text-4xl font-bold text-gray-200">{content}</h1>
     </div>
   );
 };
 
 export default NoContentFound;
+
+NoContentFound.propTypes = {
+  content: PropTypes.string.isRequired,
+};
