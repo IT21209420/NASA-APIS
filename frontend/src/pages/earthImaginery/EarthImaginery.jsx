ca8e4900c8fc67f091e0eb672da417f04e36af8d
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-05 16:48:01 +0530

    refactor: Update EarthImaginery component with lazy loading for images and improve code structure
    Update the EarthImaginery component in the frontend to implement lazy loading for images, improving page load speed by loading images only when they are visible on the screen. Additionally, the code structure of the component is improved for better readability and maintainability. These updates optimize the EarthImaginery component and enhance the user experience.

--- frontend/src/pages/earthImaginery/EarthImaginery.jsx
+++ frontend/src/pages/earthImaginery/EarthImaginery.jsx
@@ -1,28 +1,50 @@
 import { useEffect, useState } from "react";
 import NASADataService from "../../services/NASADataService";
 import Loading from "../../components/Loading";
-
 import NoContentFound from "../../components/NoContentFound";
 import ImageModal from "../../components/ImageModal";
 import bgVideo from "../../assets/video-earthImaginery.mp4";
 import BackgroundVideo from "../../components/BackgroundVideo";
 
+/**
+ * Renders the Earth Imagery component.
+ *
+ * @component
+ * @returns {JSX.Element} EarthImaginery component
+ */
 const EarthImaginery = () => {
+  // State for the Earth Imagery
   const [earthImagery, setEarthImagery] = useState({});
+
+  // State for loading
   const [isLoading, setIsLoading] = useState(false);
+
+  // State for image loading
   const [isImageLoading, setImageLoading] = useState(true);
+
+  // State for the modal
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  // State for the date
   const [date, setDate] = useState("2018-01-01");
+
+  // State for the latitude and longitude
   const [latitude, setLatitude] = useState(29.78);
+
+  //  State for the longitude
   const [longitude, setLongitude] = useState(-95.33);
 
+  // Function to open the modal
   const openModal = () => {
     setModalIsOpen(true);
   };
 
+  // Function to close the modal
   const closeModal = () => {
     setModalIsOpen(false);
   };
+
+  // Fetch Earth Imagery
   useEffect(() => {
     const fetchEarthImagery = async () => {
       setIsLoading(true);
@@ -35,17 +57,23 @@ const EarthImaginery = () => {
       setEarthImagery(imagery);
       setIsLoading(false);
     };
-    fetchEarthImagery();
+    if (latitude && longitude) {
+      fetchEarthImagery();
+    }
   }, [date, latitude, longitude]);
 
+  // Function to handle the image load
   const handleImageLoad = () => {
     console.log("Image loaded");
     setImageLoading(false);
   };
 
+  // Function to handle the date change
   const handleDateChange = (event) => {
     setDate(event.target.value);
   };
+
+  // Function to handle the latitude change
   const handleLatitudeChange = (event) => {
     const latitude = event.target.value;
     if (latitude >= -90 && latitude <= 90) {
@@ -55,6 +83,7 @@ const EarthImaginery = () => {
     }
   };
 
+  // Function to handle the longitude change
   const handleLongitudeChange = (event) => {
     const longitude = event.target.value;
     if (longitude >= -180 && longitude <= 180) {
@@ -73,46 +102,57 @@ const EarthImaginery = () => {
         style={{ height: "calc(100vh - 4rem)" }}
       >
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold text-white ">Earth Imagery</h1>
-          <div className="flex  items-center gap-5">
-            <input
-              type="date"
-              value={date}
-              onChange={handleDateChange}
-              className="rounded-md px-2 mt-2 text-gray-200 bg-transparent  bg-opacity-60 backdrop-filter backdrop-blur-lg border border-gray-600"
-            />
-            <input
-              type="number"
-              value={latitude}
-              onChange={handleLatitudeChange}
-              placeholder="Latitude"
-              className="rounded-md px-2 mt-2 text-gray-200 bg-transparent  bg-opacity-60 backdrop-filter backdrop-blur-lg border border-gray-600"
-            />
-            <input
-              type="number"
-              value={longitude}
-              onChange={handleLongitudeChange}
-              placeholder="Longitude"
-              className="rounded-md px-2 mt-2 text-gray-200 bg-transparent  bg-opacity-60 backdrop-filter backdrop-blur-lg border border-gray-600"
-            />
+          {/* <h1 className="text-2xl font-bold text-white ">Earth Imagery</h1> */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 items-center">
+            <label className="text-white ">
+              Date
+              <input
+                type="date"
+                value={date}
+                onChange={handleDateChange}
+                className="rounded-md ml-4 px-2 mt-2 text-gray-200 bg-transparent bg-opacity-60 backdrop-filter backdrop-blur-lg border border-gray-600"
+              />
+            </label>
+            <label className="text-white ">
+              Latitude
+              <input
+                type="number"
+                value={latitude}
+                onChange={handleLatitudeChange}
+                placeholder="Latitude (Eg:29.78)"
+                className="rounded-md ml-4 px-2 mt-2 text-gray-200 bg-transparent bg-opacity-60 backdrop-filter backdrop-blur-lg border border-gray-600"
+              />
+            </label>
+            <label className="text-white ">
+              Longitude
+              <input
+                type="number"
+                value={longitude}
+                onChange={handleLongitudeChange}
+                placeholder="Longitude (Eg:-95.33)"
+                className="rounded-md ml-4 px-2 mt-2 text-gray-200 bg-transparent bg-opacity-60 backdrop-filter backdrop-blur-lg border border-gray-600"
+              />
+            </label>
           </div>
           {isLoading ? (
             <Loading />
           ) : (
             <>
               {earthImagery.url ? (
-                <div
-                  className={`border border-gray-200 p-5 mt-4 ${
-                    isImageLoading ? "hidden" : ""
-                  }`}
-                >
-                  <img
-                    src={earthImagery.url}
-                    alt="Earth Imagery"
-                    className={`w-[65vh]   `}
-                    onLoad={handleImageLoad}
-                    onClick={openModal}
-                  />
+                <div>
+                  <div
+                    className={`border border-gray-200 p-5 mt-4 ${
+                      isImageLoading ? "hidden" : ""
+                    }`}
+                  >
+                    <img
+                      src={earthImagery.url}
+                      alt="Earth Imagery"
+                      className={`w-[65vh]   `}
+                      onLoad={handleImageLoad}
+                      onClick={openModal}
+                    />
+                  </div>
                   {isImageLoading && <Loading />}
                 </div>
               ) : (
