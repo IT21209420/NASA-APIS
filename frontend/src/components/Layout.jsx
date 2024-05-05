71e2f938643b2e765daa18bb950d0d3a70a345c3
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-05 08:02:30 +0530

    refactor: Update Layout component styling and child prop validation
    Update the Layout component in the frontend to improve its styling and add prop validation for the 'children' prop. This change includes modifying the font and screen height, as well as adding PropTypes validation for the 'children' prop. The updated styling enhances the visual appearance of the component, while the prop validation improves the maintainability and reliability of the codebase.

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,15 +1,23 @@
-import { useLocation } from 'react-router-dom';
-import Header from './Header';
+import PropTypes from "prop-types"; // Import PropTypes
+import { useLocation } from "react-router-dom";
+
+import Header from "./Header";
 
 const Layout = ({ children }) => {
   const location = useLocation();
-
   return (
-    <>
-      {location.pathname !== '/login' && location.pathname !=='/register' && <Header />}
-      <div className='font-mono'>{children}</div>
-    </>
+    <div className="font-mono h-screen  ">
+      {location.pathname !== "/login" && location.pathname !== "/register" && (
+        <Header />
+      )}
+
+      <div>{children}</div>
+    </div>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired, // Add prop validation for 'children'
 };
 
 export default Layout;
