ebc5f77c071d67014bbefb66e2026aa1de31ae03
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-05 08:03:31 +0530

    refactor: Add email and password fields to Register component
    Update the Register component in the frontend to include email and password fields for user registration. This change adds state variables for email and password, as well as an event handler for form submission. The handleSubmit function prevents the default form submission behavior and calls the registerUser function from the AuthContext, passing in the email and password values. This update enhances the functionality of the Register component and improves the user registration process.

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { useContext, useState } from "react";
 import BackgroundVideo from "../../components/BackgroundVideo";
 import bgVideo from "../../assets/video-login.mp4";
+import AuthContext from "../../context/AuthContext";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { registerUser } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    registerUser({ email, password });
+  };
+
   return (
     <BackgroundVideo url={bgVideo}>
       <div className="flex justify-center items-center  h-screen">
@@ -14,7 +24,7 @@ const Register = () => {
           </div>
           <form className="mt-8 space-y-6">
             <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
+            <div className="rounded-md shadow-sm  ">
               <div>
                 <label htmlFor="email-address" className="sr-only">
                   Email address
@@ -27,9 +37,11 @@ const Register = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-dominant focus:border-dominant focus:z-10 sm:text-sm"
                   placeholder="Email address"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                 />
               </div>
-              <div>
+              <div className="mt-5">
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
@@ -41,13 +53,16 @@ const Register = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-dominant focus:border-dominant focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
                 />
               </div>
             </div>
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-dominant hover:bg-pink focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-dominant"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={(e) => handleSubmit(e)}
               >
                 Sign up
               </button>
