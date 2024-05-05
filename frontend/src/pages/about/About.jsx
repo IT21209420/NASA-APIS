7687c4522d4374d84fb403d96955f6ec779006a6
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-05 17:13:28 +0530

    refactor: Implement lazy loading for images in multiple components
    Implement lazy loading for images in multiple components to improve page load speed by loading images only when they are visible on the screen. This optimization enhances the user experience and ensures efficient resource usage.

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -12,19 +12,19 @@ const About = () => {
           <p className="text-white text-xl text-center">
             Embark on an interstellar journey with us. Our web portal is a
             gateway to the marvels of the universe, brought to life through the
-            lenses of NASA's Mars rovers and Earth observation satellites.
+            lenses of NASA&apos;s Mars rovers and Earth observation satellites.
             Crafted with the latest web technologies, including React and
             Tailwind CSS, our platform offers an immersive experience that
             transcends the confines of our planet.
           </p>
           <p className="text-white text-xl text-center mt-4">
-            With real-time data powered by NASA's APIs, we present a collection
-            of breathtaking images and insights from Mars and Earth. Our
-            application's responsive design ensures seamless access across all
-            devices, while Firebase integration provides a robust backend
-            infrastructure. Join us in exploring the unknown, as we unravel the
-            mysteries of space through stunning visuals and captivating
-            discoveries.
+            With real-time data powered by NASA&apos;s APIs, we present a
+            collection of breathtaking images and insights from Mars and Earth.
+            Our application&apos;s responsive design ensures seamless access
+            across all devices, while Firebase integration provides a robust
+            backend infrastructure. Join us in exploring the unknown, as we
+            unravel the mysteries of space through stunning visuals and
+            captivating discoveries.
           </p>
         </div>
       </div>
